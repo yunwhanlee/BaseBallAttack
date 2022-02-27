6d5c9ec9c332f15dd41f3414894bae3a70fdbc09
yunwhanlee <yunwhanlee@gmail.com>
2022-02-27 23:11:42 +0900

    (BUG対応)：HitArea前にUnHitAreaボックス生成し、ベット降ってもボールがまだHitAreaに届かなかったら、player.doSwingをfalseに戻す。

--- Assets/Script/Model/BallShooter/Ball/Ball_Prefab.cs
+++ Assets/Script/Model/BallShooter/Ball/Ball_Prefab.cs
@@ -7,8 +7,11 @@ public class Ball_Prefab : MonoBehaviour
     //* OutSide
     public GameManager gm;
     public Player player;
-    [SerializeField]private int speed = 10;
+
+    //* Value
     private const int MAX_HIT_DEG = 45;
+    private bool isHited = false;
+    private int speed;
 
     Rigidbody rigid;
     void Start()
@@ -26,7 +29,7 @@ public class Ball_Prefab : MonoBehaviour
         float startPosZ = gm.hitRangeStartTf.position.z;
         float endPosZ = gm.hitRangeEndTf.position.z;
         
-        if(endPosZ <= this.transform.position.z && this.transform.position.z <= startPosZ){
+        if(!isHited && endPosZ <= this.transform.position.z && this.transform.position.z <= startPosZ){
             float offset = Mathf.Abs(startPosZ);
             float max = Mathf.Abs(endPosZ) - offset;
             float v = Mathf.Abs(this.transform.position.z) - offset;
@@ -35,29 +38,28 @@ public class Ball_Prefab : MonoBehaviour
     }
 
     private void OnTriggerStay(Collider col) {
-        if(player.doSwing){
-            if(col.gameObject.tag == "HitRangeArea"){
-                float v = gm.hitBoxDegSlider.value;
-                float deg = (MAX_HIT_DEG * 2); //真ん中がMAXになり、始めと終わりは0になるため。
-                if(v <= 0.5f){deg = -Mathf.Abs(MAX_HIT_DEG - (v * deg));}
-                else{deg = Mathf.Abs(MAX_HIT_DEG - (v * deg));}
+        if(col.gameObject.tag == "HitRangeArea"){
+            float v = gm.hitBoxDegSlider.value;
+            float deg = (MAX_HIT_DEG * 2); //真ん中がMAXになり、始めと終わりは0になるため。
+            if(v <= 0.5f){deg = -Mathf.Abs(MAX_HIT_DEG - (v * deg));}
+            else{deg = Mathf.Abs(MAX_HIT_DEG - (v * deg));}
 
-                if(player.doSwing){
-                    Debug.Log("Player:: doSwing=" + player.doSwing);
-                    Debug.Log("Ball_Prefab:: ■hitRangeSlider.v=" + v.ToString("N2") + ", ■deg=" + deg.ToString("N1"));
-                    player.doSwing = false;
-
-                    Vector3 dir = new Vector3(deg/MAX_HIT_DEG,0,1); // -45 ~ 45°
-                    float degAbs = Mathf.Abs(deg);
-                    float power = (35 < degAbs)? 1.5f : (25 < degAbs)? 2.25f : (15 < degAbs)? 3 : (7.5f < degAbs)? 4.5f : 6;
-                    Debug.Log("Ball_Prefab:: deg=" + deg + ", power=" + power);
-                    rigid.velocity = Vector3.zero;
-                    rigid.AddForce((dir).normalized * speed * power, ForceMode.Impulse);
-                }
-            }
-            else if(col.gameObject.tag == "Untagged"){
+            if(player.doSwing){
+                Debug.Log("Player:: doSwing=" + player.doSwing);
+                Debug.Log("Ball_Prefab:: ■hitRangeSlider.v=" + v.ToString("N2") + ", ■deg=" + deg.ToString("N1"));
+                isHited = true;
                 player.doSwing = false;
+
+                Vector3 dir = new Vector3(deg/MAX_HIT_DEG,0,1); // -45 ~ 45°
+                float degAbs = Mathf.Abs(deg);
+                float power = (35 < degAbs)? 1.5f : (25 < degAbs)? 2.25f : (15 < degAbs)? 3 : (7.5f < degAbs)? 4.5f : 6;
+                Debug.Log("Ball_Prefab:: deg=" + deg + ", power=" + power);
+                rigid.velocity = Vector3.zero;
+                rigid.AddForce((dir).normalized * speed * power, ForceMode.Impulse);
             }
+        }
+        else if(col.gameObject.tag == "Untagged"){
+            player.doSwing = false;
         }
     }
 
