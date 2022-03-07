79702113e5079f6a460fdcc3378680e43f31d533
yunwhanlee <yunwhanlee@gmail.com>
2022-03-08 02:52:09 +0900

    角度に向けて発射

--- Assets/Script/Model/BallShooter/Ball/Ball_Prefab.cs
+++ Assets/Script/Model/BallShooter/Ball/Ball_Prefab.cs
@@ -55,35 +55,37 @@ public class Ball_Prefab : MonoBehaviour
     //** Control
     private void OnTriggerStay(Collider col) {
         //* Batting
-        // if(col.gameObject.tag == "HitRangeArea"){
-        //     pl.setSwingArcColor("red");
-        //     float deg = calcHitRangeToDegree();
-        //     if(pl.doSwing){
-        //         // Debug.Log("Player:: doSwing=" + pl.doSwing);
-        //         // Debug.Log("Ball_Prefab:: hitRangeSlider.value=" + gm.hitRangeDegSlider.value.ToString("N2") + ", deg=" + deg.ToString("N1"));
-        //         isHited = true;
-        //         pl.doSwing = false;
+        if(col.gameObject.tag == "HitRangeArea"){
+            pl.setSwingArcColor("red");
+            //float deg = calcHitRangeToDegree();
+            if(pl.doSwing){
+                // Debug.Log("Player:: doSwing=" + pl.doSwing);
+                // Debug.Log("Ball_Prefab:: hitRangeSlider.value=" + gm.hitRangeDegSlider.value.ToString("N2") + ", deg=" + deg.ToString("N1"));
+                isHited = true;
+                pl.doSwing = false;
 
-        //         //offset Axis
-        //         const int leftSide = -1, rightSide = 1;
-        //         int sign = pl.transform.localScale.x < 0 ? leftSide : rightSide;
-        //         int offsetAxis = (sign < 0) ? (pl.MAX_HIT_DEG/2) * leftSide : (pl.MAX_HIT_DEG/2) * rightSide;
-        //         // Debug.Log("Ball_Prefab:: ■offsetAxis=" + offsetAxis);
+                //offset Axis
+                const int leftSide = -1, rightSide = 1;
+                int sign = pl.transform.localScale.x < 0 ? leftSide : rightSide;
+                int offsetAxis = (sign < 0) ? (pl.MAX_HIT_DEG/2) * leftSide : (pl.MAX_HIT_DEG/2) * rightSide;
+                // Debug.Log("Ball_Prefab:: ■offsetAxis=" + offsetAxis);
 
-        //         //Vector3 dir = new Vector3((deg / pl.MAX_HIT_DEG) * sign,0,1); // -45 ~ 45°
-        //         Vector3 dir = Vector3.forward;
-        //         dir = Quaternion.AngleAxis(deg, Vector3.up) * dir;
+                //Vector3 dir = new Vector3((deg / pl.MAX_HIT_DEG) * sign,0,1); // -45 ~ 45°
+                //Vector3 dir = Vector3.forward;
+                float deg = pl.hitAxisArrow.transform.eulerAngles.y;
+                Vector3 dir = new Vector3(Mathf.Sin(Mathf.Deg2Rad * deg), 0, Mathf.Cos(Mathf.Deg2Rad * deg)).normalized;
+                //dir = Quaternion.AngleAxis(deg, Vector3.up) * dir;
                 
-        //         float degAbs = Mathf.Abs(deg);
-        //         float power = (35 < degAbs)? 2f : (25 < degAbs)? 2.25f : (15 < degAbs)? 2.5f : (7.5f < degAbs)? 2.75f : 3f;
-        //         // Debug.Log("Ball_Prefab:: deg=" + deg + ", power=" + power);
-        //         rigid.velocity = Vector3.zero;
-        //         rigid.AddForce((dir).normalized * speed * power, ForceMode.Impulse);
-        //     }
-        // }
-        // else if(col.gameObject.tag == "Untagged"){
-        //     pl.doSwing = false;
-        // }
+                //float degAbs = Mathf.Abs(deg);
+                float power = 3f;//(35 < degAbs)? 2f : (25 < degAbs)? 2.25f : (15 < degAbs)? 2.5f : (7.5f < degAbs)? 2.75f : 3f;
+                // Debug.Log("Ball_Prefab:: deg=" + deg + ", power=" + power);
+                rigid.velocity = Vector3.zero;
+                rigid.AddForce((dir).normalized * speed * power, ForceMode.Impulse);
+            }
+        }
+        else if(col.gameObject.tag == "Untagged"){
+            pl.doSwing = false;
+        }
     }
 
     private void OnTriggerExit(Collider col) {
@@ -113,18 +115,18 @@ public class Ball_Prefab : MonoBehaviour
         speed = v;
     }
     //----------------------------------------
-    private float calcHitRangeToDegree(){
-        //Degree
-        float v = gm.hitRangeDegSlider.value;//0 ~ 1値
-        float deg = (pl.MAX_HIT_DEG * 2); //真ん中がMAXになり、始めと終わりは0になるため。
-        if(v <= 0.5f){deg = -Mathf.Abs(pl.MAX_HIT_DEG - (v * deg));}
-        else{deg = Mathf.Abs(pl.MAX_HIT_DEG - (v * deg));}
-        //SetHitAxisDir
-        const int leftSide = -1, rightSide = 1;
-        int sign = pl.transform.localScale.x < 0 ? leftSide : rightSide;
-        //Start Degree Offset
-        float offset = (sign < 0) ? (pl.offsetHitDeg) * leftSide : (pl.offsetHitDeg) * rightSide;
+    // private float calcHitRangeToDegree(){
+    //     //Degree
+    //     float v = gm.hitRangeDegSlider.value;//0 ~ 1値
+    //     float deg = (pl.MAX_HIT_DEG * 2); //真ん中がMAXになり、始めと終わりは0になるため。
+    //     if(v <= 0.5f){deg = -Mathf.Abs(pl.MAX_HIT_DEG - (v * deg));}
+    //     else{deg = Mathf.Abs(pl.MAX_HIT_DEG - (v * deg));}
+    //     //SetHitAxisDir
+    //     const int leftSide = -1, rightSide = 1;
+    //     int sign = pl.transform.localScale.x < 0 ? leftSide : rightSide;
+    //     //Start Degree Offset
+    //     float offset = (sign < 0) ? (pl.offsetHitDeg) * leftSide : (pl.offsetHitDeg) * rightSide;
 
-        return deg * sign + offset;
-    }
+    //     return deg * sign + offset;
+    // }
 }
