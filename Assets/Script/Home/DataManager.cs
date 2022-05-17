eff4b4d2162522395c911d466015ddeb258be9fd
DESKTOP-4QECMS1\lee <yunwhan-lee@sims.co.jp>
2022-05-17 16:24:27 +0900

    Home Scene : Select Charactors Regist

---
+++ Assets/Script/Home/DataManager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class DataManager : MonoBehaviour
+{
+    
+    public static DataManager ins;
+
+    [Header("--Select Charactor--")]
+    public Transform scrollContentTf;
+    public RectTransform charaRectTf;
+    public GameObject[] charaPfs;
+    public int selectCharaIdx = 0;
+    void Awake() => singleton();
+    void Start()
+    {
+        //* Charactors Regist
+        Array.ForEach(charaPfs, chara=>{
+            Transform parentTf = Instantiate(charaRectTf, charaRectTf.localPosition, charaRectTf.localRotation, scrollContentTf).transform;
+            Instantiate(chara, Vector3.zero, Quaternion.identity, parentTf);
+        });
+    }
+
+    void Update()
+    {
+    }
+
+    void singleton(){
+        //* Singleton
+        if(ins == null) ins = this;
+        else if(ins != null) return;
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
