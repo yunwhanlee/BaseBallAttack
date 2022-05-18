1765cdf28f5a3ed2760b5a873f6875c46d28c441
DESKTOP-4QECMS1\lee <yunwhan-lee@sims.co.jp>
2022-05-18 13:29:57 +0900

    ScrollView set Scroll Speed & Stop Scrolling To Near Chara PosX.

--- Assets/Script/Home/DataManager.cs
+++ Assets/Script/Home/DataManager.cs
@@ -8,10 +8,10 @@ public class DataManager : MonoBehaviour
     public static DataManager ins;
 
     [Header("--Select Charactor--")]
-    public RectTransform scrollContentTf; public RectTransform ScrollContentTf {get => scrollContentTf; set => scrollContentTf = value;}
-    public RectTransform charaParentTf;
-    public GameObject[] charaPfs;
-    public int selectCharaIdx = 0;
+    [SerializeField] RectTransform scrollContentTf; public RectTransform ScrollContentTf {get => scrollContentTf; set => scrollContentTf = value;}
+    [SerializeField] RectTransform charaParentTf;   public RectTransform CharaParentTf {get => charaParentTf; set => charaParentTf = value;}
+    [SerializeField] GameObject[] charaPfs; public GameObject[] CharaPfs {get => charaPfs; set => charaPfs = value;}
+    [SerializeField] int selectCharaIdx = 0;  public int SelectCharaIdx {get => selectCharaIdx; set => selectCharaIdx = value;}
     void Awake() => singleton();
     void Start()
     {
--- Assets/Script/Home/ScrollViewEvent.cs
+++ Assets/Script/Home/ScrollViewEvent.cs
@@ -6,34 +6,51 @@ using UnityEngine.EventSystems;
 
 public class ScrollViewEvent : MonoBehaviour, IBeginDragHandler, IEndDragHandler
 {
-    private float rectWidth;
-    private float curIdxBasePos;
+    float rectWidth;
+    float curIdxBasePos;
+
+    [Header("--Scroll Speed--")]
+    [SerializeField] float scrollStopSpeed;
+    float scrollSpeed;
+    float scrollBefFramePosX;
+
+    
 
     void Start(){
-        rectWidth = DataManager.ins.charaParentTf.rect.width;
-        Debug.Log("ScrollViewEvent:: rectWidth= " + rectWidth);
+        rectWidth = DataManager.ins.CharaParentTf.rect.width;
     }
 
     //* Drag Event
     public void OnBeginDrag(PointerEventData eventData){
-        Debug.Log("<color=white> ScrollViewEvent:: Drag Begin </color>");
+        // Debug.Log("<color=white> ScrollViewEvent:: Drag Begin </color>");
     }
 
     public void OnEndDrag(PointerEventData eventData){
-        int posX = (int)DataManager.ins.ScrollContentTf.anchoredPosition.x;
-        Debug.Log("<color=white> ScrollViewEvent:: Drag End:: curIdxPos=" + curIdxBasePos + ", ScrollContentTf.PosX=" + posX + "</color>");
-        DataManager.ins.ScrollContentTf.anchoredPosition = new Vector2(curIdxBasePos, -500);
+        // int posX = (int)DataManager.ins.ScrollContentTf.anchoredPosition.x;
+        // Debug.Log("<color=white> ScrollViewEvent:: Drag End:: curIdxPos=" + curIdxBasePos + ", ScrollContentTf.PosX=" + posX + "</color>");
     }
 
     public void OnScrollViewPos(RectTransform pos){ //* －が右側
-        // Debug.Log("Scroll View Pos =" + pos.anchoredPosition.x + ", " + pos.anchoredPosition.y); // 0 ~ 1
         float width = Mathf.Abs(rectWidth);
         float offset = -(width + width/2);
         float curPosX = pos.anchoredPosition.x - offset;
-        float max = width * DataManager.ins.charaPfs.Length - width;
+        float max = width * DataManager.ins.CharaPfs.Length - width;
         int idx = Mathf.Abs(Mathf.FloorToInt((curPosX) / width));
         curIdxBasePos = -((idx+1) * width);
-        // Debug.Log("getScrollViewPos:: curPosX=" + (curPosX) + " / " + max + ", idx=" + idx + " (curIdxPos=" + curIdxPos + ")");
-        
+
+        //* Scroll Speed
+        scrollSpeed = Mathf.Abs(scrollBefFramePosX - pos.anchoredPosition.x);
+        Debug.Log(scrollBefFramePosX + " - " + pos.anchoredPosition.x + " = " + scrollSpeed);
+
+        //* Stop Scrolling To Near Chara PosX
+        if(scrollSpeed < 1){
+            DataManager.ins.ScrollContentTf.anchoredPosition = new Vector2(curIdxBasePos, -500);
+        }
+
+        //* update Before Frame PosX
+        scrollBefFramePosX = pos.anchoredPosition.x;
+
+        // Debug.Log("getScrollViewPos:: curPosX=" + (curPosX) + " / " + max + ", idx=" + idx + " (curIdxBasePos=" + curIdxBasePos + "), scrollSpeed=" + scrollSpeed);
     }
+
 }
