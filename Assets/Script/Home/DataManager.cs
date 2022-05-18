c878f247bed6f27aa96bdf18988002634d55b6ed
DESKTOP-4QECMS1\lee <yunwhan-lee@sims.co.jp>
2022-05-18 15:25:38 +0900

    setScrollStopCharaInfo() not Yet Price

--- Assets/Script/Home/DataManager.cs
+++ Assets/Script/Home/DataManager.cs
@@ -12,6 +12,7 @@ public class DataManager : MonoBehaviour
     [SerializeField] RectTransform charaParentTf;   public RectTransform CharaParentTf {get => charaParentTf; set => charaParentTf = value;}
     [SerializeField] GameObject[] charaPfs; public GameObject[] CharaPfs {get => charaPfs; set => charaPfs = value;}
     [SerializeField] int selectCharaIdx = 0;  public int SelectCharaIdx {get => selectCharaIdx; set => selectCharaIdx = value;}
+    [SerializeField] string selectCharaName = ""; public string SelectCharaName {get => selectCharaName; set => selectCharaName = value;}
     void Awake() => singleton();
     void Start()
     {
--- Assets/Script/Home/ScrollViewEvent.cs
+++ Assets/Script/Home/ScrollViewEvent.cs
@@ -8,11 +8,16 @@ public class ScrollViewEvent : MonoBehaviour, IBeginDragHandler, IEndDragHandler
 {
     float rectWidth;
     float curIdxBasePos;
+    int curIdx;
 
     [Header("--Scroll Speed--")]
     [SerializeField] float scrollStopSpeed;
     float scrollSpeed;
     float scrollBefFramePosX;
+
+    [Header("--UI--")]
+    public Text nameTxt;
+
 
     
 
@@ -26,8 +31,13 @@ public class ScrollViewEvent : MonoBehaviour, IBeginDragHandler, IEndDragHandler
     }
 
     public void OnEndDrag(PointerEventData eventData){
-        // int posX = (int)DataManager.ins.ScrollContentTf.anchoredPosition.x;
-        // Debug.Log("<color=white> ScrollViewEvent:: Drag End:: curIdxPos=" + curIdxBasePos + ", ScrollContentTf.PosX=" + posX + "</color>");
+        //* (BUG) スクロールが先端と末端だったら、イベント起動しない。
+        int posX = (int)DataManager.ins.ScrollContentTf.anchoredPosition.x;
+        Debug.Log("<color=white> ScrollViewEvent:: Drag End:: PosX=" + posX + ", curIdx=" + curIdx + "Charas.Len=" + (DataManager.ins.CharaPfs.Length - 1) + "</color>");
+
+        if(curIdx == 0 || curIdx == DataManager.ins.CharaPfs.Length-1){
+            setScrollStopCharaInfo();
+        }
     }
 
     public void OnScrollViewPos(RectTransform pos){ //* －が右側
@@ -35,22 +45,33 @@ public class ScrollViewEvent : MonoBehaviour, IBeginDragHandler, IEndDragHandler
         float offset = -(width + width/2);
         float curPosX = pos.anchoredPosition.x - offset;
         float max = width * DataManager.ins.CharaPfs.Length - width;
-        int idx = Mathf.Abs(Mathf.FloorToInt((curPosX) / width));
-        curIdxBasePos = -((idx+1) * width);
+        curIdx = Mathf.Abs(Mathf.FloorToInt((curPosX) / width));
+        curIdxBasePos = -((curIdx+1) * width);
 
         //* Scroll Speed
         scrollSpeed = Mathf.Abs(scrollBefFramePosX - pos.anchoredPosition.x);
-        Debug.Log(scrollBefFramePosX + " - " + pos.anchoredPosition.x + " = " + scrollSpeed);
+        // Debug.Log(scrollBefFramePosX + " - " + pos.anchoredPosition.x + " = " + scrollSpeed);
 
-        //* Stop Scrolling To Near Chara PosX
+        //* Stop Scrolling Near Index Chara
         if(scrollSpeed < 1){
-            DataManager.ins.ScrollContentTf.anchoredPosition = new Vector2(curIdxBasePos, -500);
+            setScrollStopCharaInfo();
+            // Debug.Log("getScrollViewPos:: Stop Scrolling:: curPosX=" + (curPosX) + " / " + max + ", idx=" + curIdx + " (curIdxBasePos=" + curIdxBasePos + "), scrollSpeed=" + scrollSpeed);
         }
 
         //* update Before Frame PosX
         scrollBefFramePosX = pos.anchoredPosition.x;
-
-        // Debug.Log("getScrollViewPos:: curPosX=" + (curPosX) + " / " + max + ", idx=" + idx + " (curIdxBasePos=" + curIdxBasePos + "), scrollSpeed=" + scrollSpeed);
     }
 
+    private void setScrollStopCharaInfo(){
+        //* Set PosX
+        DataManager.ins.ScrollContentTf.anchoredPosition = new Vector2(curIdxBasePos, -500);
+        
+        //* Set Name
+        string objName = DataManager.ins.CharaPfs[curIdx].name;
+        string charaName = objName.Split('_')[1];
+        nameTxt.text = charaName;
+
+        //* Set Price
+        
+    }
 }
